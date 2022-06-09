531f319a16cd5a283362ae816857632656af8ab4
Citylandscape <107222176+Citylandscape@users.noreply.github.com>
2022-06-09 19:53:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbz7wn88ncfK2b4P4dkOByUQ_JIpksSuHJLfQ9c9aawuTLLx8dLu0zz3rr1rHMnngHCppw/exec";
